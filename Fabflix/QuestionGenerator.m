88b953b9cf930796aa5d97fcc85fe4e64cf94bf5
Neal Young <hi@nealyoung.me>
2013-03-24 23:11:49 -0700

    Improved readme

--- Fabflix/QuestionGenerator.m
+++ Fabflix/QuestionGenerator.m
@@ -26,15 +26,13 @@
         [database open];
     }
     
-    return self; 
+    return self;
 }
 
 - (QuizQuestion *)newQuestion {
     QuizQuestion *currentQuestion;
     int questionType = arc4random() % 10;
-            
-    NSLog(@"%d", questionType);
-    
+        
     if (questionType == 0) {
         currentQuestion = [self getQuestionTypeDirector];
     } else if (questionType == 1) {
@@ -65,7 +63,7 @@
     
     FMResultSet *incorrect = [database executeQuery:@"SELECT director FROM movies WHERE director!=? ORDER BY RANDOM() LIMIT 3;", movie.director];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]){
         NSString *name = [incorrect stringForColumn:@"director"];
         [answerOptions addObject:name];
@@ -82,14 +80,14 @@
     
     FMResultSet *correct = [database executeQuery:@"SELECT year FROM movies WHERE id=?;", [NSNumber numberWithInt:movie.id]];
     NSString *answer;
-
+    
     while ([correct next]) {
         answer = [NSString stringWithFormat:@"%d", [correct intForColumn:@"year"]];
     }
     
     FMResultSet *incorrect = [database executeQuery:@"SELECT DISTINCT year FROM movies WHERE year!=? ORDER BY RANDOM() LIMIT 3;", answer];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         [answerOptions addObject:[NSString stringWithFormat:@"%d", [incorrect intForColumn:@"year"]]];
     }
@@ -114,11 +112,11 @@
         correctLastName = [correct stringForColumn:@"last_name"];
         star = [NSString stringWithFormat:@"%@ %@", correctFirstName, correctLastName];
     }
-
-        
+    
+    
     FMResultSet *incorrect = [database executeQuery:@"SELECT first_name, last_name FROM stars, stars_in_movies WHERE first_name!=? AND stars.last_name!=? AND stars_in_movies.star_id=stars.id AND stars_in_movies.movie_id!=? ORDER BY RANDOM() LIMIT 3;", correctFirstName, correctLastName, [NSNumber numberWithInteger:movie.id]];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         [answerOptions addObject:[NSString stringWithFormat:@"%@ %@", [incorrect stringForColumn:@"first_name"], [incorrect stringForColumn:@"last_name"]]];
     }
@@ -129,22 +127,22 @@
     return quizQuestion;
 }
 
-- (QuizQuestion *)getQuestionTypeStarNotInMovie {    
+- (QuizQuestion *)getQuestionTypeStarNotInMovie {
     QuizItem *movie = [self randomMovieWithMinimumStars:3];
-        
+    
     FMResultSet *correct = [database executeQuery:@"SELECT stars.first_name, stars.last_name, stars.id AS s FROM stars WHERE NOT EXISTS (SELECT stars.id FROM stars, stars_in_movies WHERE stars_in_movies.star_id=s AND stars_in_movies.movie_id=?) ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:movie.id]];
-        
+    
     NSString *answer;
     
     if ([correct next]) {
         answer = [NSString stringWithFormat:@"%@ %@", [correct stringForColumn:@"first_name"], [correct stringForColumn:@"last_name"]];
     }
-
+    
     FMResultSet *incorrect = [database executeQuery:@"SELECT stars.first_name, stars.last_name FROM stars, stars_in_movies WHERE stars_in_movies.star_id=stars.id AND stars_in_movies.movie_id=? ORDER BY RANDOM() LIMIT 3;", [NSNumber numberWithInteger:movie.id]];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
     
     // Add incorrect names to the list of answer options
-    while ([incorrect next]) {        
+    while ([incorrect next]) {
         [answerOptions addObject:[NSString stringWithFormat:@"%@ %@", [incorrect stringForColumn:@"first_name"], [incorrect stringForColumn:@"last_name"]]];
     }
     
@@ -190,7 +188,7 @@
     QuizItem *movie = [self randomMovie];
     
     FMResultSet *correct = [database executeQuery:@"SELECT stars.first_name, stars.last_name, stars.id FROM stars, stars_in_movies WHERE stars_in_movies.movie_id=? AND stars_in_movies.star_id=stars.id ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:movie.id]];
-  
+    
     NSString *star;
     NSInteger starID = 0;
     
@@ -198,11 +196,11 @@
         star = [NSString stringWithFormat:@"%@ %@", [correct stringForColumn:@"first_name"], [correct stringForColumn:@"last_name"]];
         starID = [correct intForColumn:@"id"];
     }
-        
+    
     FMResultSet *incorrect = [database executeQuery:@"SELECT director, movies.id AS m FROM movies WHERE NOT EXISTS(SELECT star_id FROM stars_in_movies WHERE star_id=? AND movie_id=m) ORDER BY RANDOM() LIMIT 3;", [NSNumber numberWithInteger:starID]];
     
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         [answerOptions addObject:[incorrect stringForColumn:@"director"]];
     }
@@ -213,18 +211,18 @@
     return quizQuestion;
 }
 
-- (QuizQuestion *)getQuestionTypeNotDirectorForStar {    
+- (QuizQuestion *)getQuestionTypeNotDirectorForStar {
     FMResultSet *details = [database executeQuery:@"SELECT first_name, last_name, id FROM stars WHERE (SELECT COUNT(DISTINCT director) FROM stars_in_movies AS s, movies AS m WHERE stars.id=s.star_id AND m.id=s.movie_id) >= 3 ORDER BY RANDOM() LIMIT 1;"];
     
     NSString *star;
     NSInteger starID = 0;
-
+    
     if ([details next]) {
         star = [NSString stringWithFormat:@"%@ %@", [details stringForColumn:@"first_name"], [details stringForColumn:@"last_name"]];
         starID = [details intForColumn:@"id"];
     }
-        
-    FMResultSet *correct = [database executeQuery:@"SELECT director FROM movies AS m WHERE NOT EXISTS (SELECT stars_in_movies.movie_id FROM stars_in_movies WHERE stars_in_movies.movie_id=m.id AND stars_in_movies.star_id=?) ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:starID]];
+    
+    FMResultSet *correct = [database executeQuery:@"SELECT director FROM movies AS m, stars_in_movies AS s WHERE NOT EXISTS (SELECT stars_in_movies.movie_id FROM stars_in_movies WHERE stars_in_movies.movie_id=m.id AND stars_in_movies.star_id=?) ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:starID]];
     
     NSString *answer;
     
@@ -234,7 +232,7 @@
     
     FMResultSet *incorrect = [database executeQuery:@"SELECT DISTINCT director FROM movies NATURAL JOIN stars_in_movies WHERE stars_in_movies.star_id=? AND stars_in_movies.movie_id=id ORDER BY RANDOM() LIMIT 3;", [NSNumber numberWithInteger:starID]];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         NSString *director = [incorrect stringForColumn:@"director"];
         [answerOptions addObject:director];
@@ -247,7 +245,7 @@
 }
 
 - (QuizQuestion *)getQuestionTypeStarAppearBothMovies {
-    FMResultSet *star = [database executeQuery:@"SELECT stars.id, stars.first_name, stars.last_name FROM stars, stars_in_movies as s, stars_in_movies as m WHERE m.star_id = stars.id AND s.star_id = stars.id AND m.movie_id != s.movie_id ORDER BY RANDOM() LIMIT 1;"];
+    FMResultSet *star = [database executeQuery:@"SELECT stars.id, stars.first_name, stars.last_name from stars, stars_in_movies as s, stars_in_movies as m WHERE m.star_id = stars.id AND s.star_id = stars.id AND m.movie_id != s.movie_id ORDER BY RANDOM() LIMIT 1;"];
     
     NSString *answer;
     NSInteger starID = 0;
@@ -257,7 +255,7 @@
         starID = [star intForColumn:@"id"];
     }
     
-    FMResultSet *movies = [database executeQuery:@"SELECT movies.title, movies.id FROM movies, stars_in_movies WHERE stars_in_movies.star_id=? AND stars_in_movies.movie_id = movies.id ORDER BY RANDOM() LIMIT 2;", [NSNumber numberWithInteger:starID]];
+    FMResultSet *movies = [database executeQuery:@"SELECT movies.title, movies.id from movies, stars_in_movies WHERE stars_in_movies.star_id=? AND stars_in_movies.movie_id = movies.id ORDER BY RANDOM() LIMIT 2;", [NSNumber numberWithInteger:starID]];
     NSMutableArray *movieNames = [[NSMutableArray alloc] init];
     NSMutableArray *movieIds = [[NSMutableArray alloc] init];
     NSNumber *movieID;
@@ -269,9 +267,9 @@
         [movieIds addObject:movieID];
     }
     
-    FMResultSet *incorrect = [database executeQuery:@"SELECT stars.first_name, stars.last_name FROM stars, stars_in_movies WHERE stars_in_movies.star_id = stars.id AND stars_in_movies.movie_id != ? ORDER BY RANDOM() LIMIT 3;", movieID];
+    FMResultSet *incorrect = [database executeQuery:@"SELECT stars.first_name, stars.last_name from stars, stars_in_movies WHERE stars_in_movies.star_id = stars.id AND stars_in_movies.movie_id != ? ORDER BY RANDOM() LIMIT 3;", movieID];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         [answerOptions addObject:[NSString stringWithFormat:@"%@ %@", [incorrect stringForColumn:@"first_name"], [incorrect stringForColumn:@"last_name"]]];
     }
@@ -290,7 +288,7 @@
     if ([movies next]) {
         movieID = [movies intForColumn:@"id"];
     }
-
+    NSLog(@"%d", movieID);
     FMResultSet *results = [database executeQuery:@"SELECT stars.first_name, stars.last_name from stars, stars_in_movies WHERE stars_in_movies.star_id=stars.id AND stars_in_movies.movie_id = ? ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:movieID]];
     
     NSString *star;
@@ -328,7 +326,7 @@
 - (QuizQuestion *)getQuestionTypeDirectorForStarInYear {
     QuizItem *movie = [self randomMovie];
     
-    FMResultSet *details = [database executeQuery:@"SELECT stars.first_name, stars.last_name, stars.id, movies.year, movies.id FROM stars, movies, stars_in_movies WHERE stars_in_movies.star_id=stars.id AND stars_in_movies.movie_id=? AND movies.id=? ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:movie.id], [NSNumber numberWithInteger:movie.id]];
+    FMResultSet *details = [database executeQuery:@"SELECT stars.first_name, stars.last_name, stars.id, movies.year, movies.id from stars, movies, stars_in_movies WHERE stars_in_movies.star_id=stars.id AND stars_in_movies.movie_id= ? AND movies.id= ? ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:movie.id], [NSNumber numberWithInteger:movie.id]];
     
     NSString *star;
     NSInteger year, starID;
@@ -339,22 +337,22 @@
         starID = [details intForColumn:@"id"];
     }
     
-    FMResultSet *incorrect = [database executeQuery:@"SELECT movies.director FROM movies, stars_in_movies WHERE stars_in_movies.movie_id=movies.id AND stars_in_movies.star_id!=? ORDER BY RANDOM() LIMIT 3;", [NSNumber numberWithInteger:starID]];
+    FMResultSet *incorrect = [database executeQuery:@"SELECT movies.director from movies, stars_in_movies WHERE stars_in_movies.movie_id=movies.id AND stars_in_movies.star_id!=? ORDER BY RANDOM() LIMIT 3;", [NSNumber numberWithInteger:starID]];
     NSMutableArray *answerOptions = [[NSMutableArray alloc] init];
-
+    
     while ([incorrect next]) {
         [answerOptions addObject:[incorrect stringForColumn:@"director"]];
     }
     
     NSString *question = [NSString stringWithFormat:@"Who directed %@ in %d?", star, year];
     QuizQuestion *quizQuestion = [self generateQuestion:question withCorrectAnswer:movie.director incorrectAnswers:answerOptions];
-
+    
     return quizQuestion;
 }
 
 //Helper method that returns a random movie
 - (QuizItem *)randomMovie {
-    FMResultSet *result = [database executeQuery:@"SELECT id, title, director FROM movies ORDER BY RANDOM() LIMIT 1;"];
+    FMResultSet *result = [database executeQuery:@"SELECT id, title, director from movies WHERE (SELECT count(stars_in_movies.star_id) from stars_in_movies WHERE stars_in_movies.movie_id=movies.id) >= 1 ORDER BY RANDOM() LIMIT 1;"];
     
     NSInteger identifier;
     NSString *title, *director;
@@ -372,7 +370,7 @@
 
 //Helper method that returns a random movie at least the specified minimum number of stars.
 - (QuizItem *)randomMovieWithMinimumStars:(NSInteger)stars {
-    FMResultSet *result = [database executeQuery:@"SELECT id, title, director FROM movies WHERE (SELECT count(stars_in_movies.star_id) FROM stars_in_movies WHERE stars_in_movies.movie_id=movies.id) >= ? ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:stars]];
+    FMResultSet *result = [database executeQuery:@"SELECT id, title, director from movies, stars_in_movies WHERE (SELECT count(stars_in_movies.star_id) from stars_in_movies WHERE stars_in_movies.movie_id=movies.id) >= ? ORDER BY RANDOM() LIMIT 1;", [NSNumber numberWithInteger:stars]];
     
     NSInteger identifier;
     NSString *title, *director;
@@ -390,7 +388,7 @@
 
 //Helper Method that takes a the question in string format, the answer, and an array of incorrect answers.
 - (QuizQuestion *)generateQuestion:(NSString *)question withCorrectAnswer:(NSString *)answer incorrectAnswers:(NSMutableArray *)answers {
-    int answerIndex = arc4random() % 4;
+    int answerIndex = arc4random() % 3;
     [answers insertObject:answer atIndex:answerIndex];
     
     QuizQuestion *quizQuestion = [[QuizQuestion alloc] initWithQuestion:question answers:answers answerIndex:answerIndex];
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Fabflix-Quiz
 ============
 
-Fabflix Quiz is an iOS movie quiz app created for Professor Chen Li's CS122B course at UC Irvine.
+Fabflix Quiz is an iOS movie quiz app created for Professor Chen Li's CS122B course at UC Irvine. Questions are randomly generated based on a provided database of movie information.
